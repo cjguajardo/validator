df70bde6d69c63afc91571cd288e00c8ea8f3ebf
Carlos Guajardo <cj.guajardo@cgcapps.cl>
2023-11-13 22:47:04 -0300

    Refactor validator function to simplify code and improve performance

--- index.js
+++ index.js
@@ -3,74 +3,74 @@
  * @param {Object} params
  * @param {Object} rules
  */
-export default function validator(params, rules, customErrorMessages = null) {
+function validator (params, rules) {
   if (typeof params === "undefined" || typeof rules === "undefined") {
-    console.error("Validator: Debe ingresar todos los parámetros.");
-    return { messages: {}, keys: [], success: false };
+    console.error("Validator: Debe ingresar todos los parámetros.")
+    return { messages: {}, keys: [], success: false }
   }
 
   const getRuleAttributeValue = function (ruleAttribute) {
-    const parts = ruleAttribute.split("=");
-    return parts[1];
-  };
+    const parts = ruleAttribute.split("=")
+    return parts[1]
+  }
 
   const isValidEmail = (value) => {
     const re =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(value).toLowerCase());
-  };
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return re.test(String(value).toLowerCase())
+  }
 
   const isValidPhone = (value) => {
-    const re = /^\d{9}$/;
-    return re.test(String(value));
-  };
+    const re = /^\d{9}$/
+    return re.test(String(value))
+  }
 
   const isValidDate = (value) => {
     //clean in case that the user has entered a date with time
-    const cleanValue = value.replace(/\//g, "-").split(" ")[0];
-    let date = null;
+    const cleanValue = value.replace(/\//g, "-").split(" ")[0]
+    let date = null
 
-    const dmYFormat = /^\d{2}-\d{2}-\d{4}$/;
-    const YmdFormat = /^\d{4}-\d{2}-\d{2}$/;
+    const dmYFormat = /^\d{2}-\d{2}-\d{4}$/
+    const YmdFormat = /^\d{4}-\d{2}-\d{2}$/
 
     if (dmYFormat.test(String(cleanValue))) {
-      const parts = cleanValue.split("-");
-      date = new Date(`${parts[2]}-${parts[1]}-${parts[0]}`);
+      const parts = cleanValue.split("-")
+      date = new Date(`${parts[2]}-${parts[1]}-${parts[0]}`)
     } else if (YmdFormat.test(String(cleanValue))) {
-      date = new Date(cleanValue);
+      date = new Date(cleanValue)
     }
-    return date && !isNaN(date.getTime());
-  };
+    return date && !isNaN(date.getTime())
+  }
 
   const isValidRut = (value) => {
-    value = value.replace(/\./g, "");
-    return /^[0-9]+[-|‐]{1}[0-9kK]{1}$/.test(String(value));
-  };
+    value = value.replace(/\./g, "")
+    return /^[0-9]+[-|‐]{1}[0-9kK]{1}$/.test(String(value))
+  }
 
-  let messages = {};
-  let keys = [];
+  let messages = {}
+  let keys = []
 
   for (const key in rules) {
     let message = `Debe ingresar __KEY__`,
       required = false,
       alias = key,
       _in = null,
-      type = "any";
+      type = "any"
     //in => in=string1|string2|string3
 
     //Obtener valores de reglas de validación
     if (rules[key].indexOf(";")) {
-      const ruleAttributes = rules[key].split(";");
+      const ruleAttributes = rules[key].split(";")
 
       for (const ruleAttribute of ruleAttributes) {
         if (ruleAttribute.indexOf("alias") == 0) {
-          alias = getRuleAttributeValue(ruleAttribute);
+          alias = getRuleAttributeValue(ruleAttribute)
         } else if (ruleAttribute.indexOf("required") == 0) {
-          required = true;
+          required = true
         } else if (ruleAttribute.indexOf("type") == 0) {
-          type = getRuleAttributeValue(ruleAttribute);
+          type = getRuleAttributeValue(ruleAttribute)
         } else if (ruleAttribute.indexOf("in") == 0) {
-          _in = getRuleAttributeValue(ruleAttribute);
+          _in = getRuleAttributeValue(ruleAttribute)
         }
       }
     }
@@ -79,54 +79,56 @@ export default function validator(params, rules, customErrorMessages = null) {
       const shouldValidate =
         typeof params[key] === "undefined" ||
         params[key] == null ||
-        params[key] == "";
+        params[key] == ""
 
       if (shouldValidate) {
         if (isRequired) {
-          messages[key] = message.replace("__KEY__", alias);
-          keys.push(key);
+          messages[key] = message.replace("__KEY__", alias)
+          keys.push(key)
         }
       } else {
         if (type != "any") {
-          let message = null;
+          let message = null
           if (
             (type === "number" && isNaN(params[key])) ||
             (type === "string" && !isNaN(params[key]))
           ) {
-            message = `El valor ingresado para ${alias} no es el correcto.`;
+            message = `El valor ingresado para ${alias} no es el correcto.`
           }
           if (type === "email" && !isValidEmail(params[key])) {
-            message = `El valor ingresado para ${alias} no es el correcto.`;
+            message = `El valor ingresado para ${alias} no es el correcto.`
           }
           if (type === "phone" && !isValidPhone(params[key])) {
-            message = `El valor ingresado para ${alias} no es el correcto.`;
+            message = `El valor ingresado para ${alias} no es el correcto.`
           }
           if (type === "rut" && !isValidRut(params[key])) {
-            message = `El valor ingresado para ${alias} no es el correcto. P.e: XXXXXXXX-X o XX.XXX.XXX-X`;
+            message = `El valor ingresado para ${alias} no es el correcto. P.e: XXXXXXXX-X o XX.XXX.XXX-X`
           }
           if (type === "date" && !isValidDate(params[key])) {
-            message = `El valor ingresado para ${alias} no es el correcto.`;
+            message = `El valor ingresado para ${alias} no es el correcto.`
           }
 
           if (message) {
-            messages[key] = message;
-            keys.push(key);
+            messages[key] = message
+            keys.push(key)
           }
         }
         if (_in) {
-          const inValues = _in.toLowerCase().split("|");
+          const inValues = _in.toLowerCase().split("|")
           if (!inValues.includes(params[key].toLowerCase())) {
             messages[
               key
-            ] = `El valor ingresado para ${alias} no es el correcto.`;
-            keys.push(key);
+            ] = `El valor ingresado para ${alias} no es el correcto.`
+            keys.push(key)
           }
         }
       }
-    };
+    }
 
-    validate(required);
+    validate(required)
   }
 
-  return { messages, keys, success: keys.length == 0 };
+  return { messages, keys, success: keys.length == 0 }
 }
+
+module.exports = validator
